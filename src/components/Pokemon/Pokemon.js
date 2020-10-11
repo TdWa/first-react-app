1f71ca5a4084348c8518b6123ad2b47689139308
TdWa <59452572+TdWa@users.noreply.github.com>
2020-10-11 13:41:42 +0200

    finished excercise

---
+++ src/components/Pokemon/Pokemon.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Pokemon.scss";
+
+export default function Pokemon(props) {
+  return (
+    <div className="card shadow-sm mb-4 Pokemon">
+      <div className="card-body pb-0">
+        <h5 className="card-title">Pokemon name: {props.name}</h5>
+        <h6 className="card-subtitle mb-3 text-primary">
+          {props.awesome ? "An awesome Pokemon" : "Not awesome"}
+        </h6>
+        <p className="mb-0">
+          Weight: {props.weight} kg
+          <br />
+          Terrifying: {props.terrifying ? "Very" : "nah, lovable"}
+          <br />
+          Abilities: {`(${props.abilities.length})`}
+        </p>
+      </div>
+      <ul className="list-group list-group-flush">
+        {props.abilities.map((ability, i) => {
+          return <li className="list-group-item">{ability}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/Title/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Title(props) {
+  return (
+    <div className="mb-4">
+      <h1>{props.content}</h1>
+    </div>
+  );
+}
